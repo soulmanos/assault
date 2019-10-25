3deca8473906fdc2809a697dcf24dd6ca24eced1
Alex Soul <apsoul@hotmail.com>
2019-10-25 11:39:36 +0100

    Completed Web Loadtesting Tool

--- assault/cli.py
+++ assault/cli.py
@@ -1,5 +1,10 @@
 import click
+import sys
+import json
+from typing import TextIO
+
 from .http import assault
+from .stats import Results
 
 
 @click.command()
@@ -8,12 +13,52 @@ from .http import assault
 @click.option("--json-file", "-j", default=None, help="Path to output JSON file")
 @click.argument("url")
 def cli(requests, concurrency, json_file, url):
-    print(f"Requests: {requests}")
-    print(f"Concurrency: {concurrency}")
-    print(f"JSON File: {json_file}")
-    print(f"URL: {url}")
-    assault(url, requests, concurrency)
+    # print(f"Requests: {requests}")
+    # print(f"Concurrency: {concurrency}")
+    # print(f"JSON File: {json_file}")
+    # print(f"URL: {url}")
+    output_file = None
+    if json_file:
+        try:
+            output_file = open(json_file, "w")
+        except:
+            print(f"Unable to open file: {json_file}")
+            sys.exit(1)
+    total_time, requests = assault(url, requests, concurrency)
+    results = Results(total_time, requests)  # tuple
+    display(results, output_file)
 
 
-if __name__ == "__main__":
-    cli()
+def display(results: Results, json_file: TextIO):
+    # test_dict = {"successful_requests": "10", "slowest": 2, "total_time": 5}
+    # output_file = open("nogit_test.json", "w")
+    # json.dump(test_dict, output_file)
+    # output_file.close()
+    if json_file:
+        json.dump(
+            {
+                "successful_requests": results.successful_requests(),
+                "slowest": results.slowest(),
+                "fastest": results.fastest(),
+                "total_time": results.total_time,
+                "requests_per_minute": results.requests_per_minute(),
+                "requests_per_second": results.requests_per_second(),
+            },
+            json_file,
+        )
+        json_file.close()
+        print(".... Done!")
+    else:
+        print(".... Done!")
+        print("--- Results ---")
+        print(f"Successful requests\t{results.successful_requests()}")
+        print(f"Slowest            \t{results.slowest()}")
+        print(f"Fastest            \t{results.fastest()}")
+        print(f"Average            \t{results.average_time()}")
+        print(f"Total time         \t{results.total_time}")
+        print(f"Requests Per Minute\t{results.requests_per_minute()}")
+        print(f"Requests Per Second\t{results.requests_per_second()}")
+
+
+# if __name__ == "__main__":
+#     cli()
--- assault/http.py
+++ assault/http.py
@@ -76,16 +76,18 @@ async def distribute_work(url, requests, concurrency, results):
     for task in tasks:
         task.cancel()
 
-    print("---")
-    print(
-        f"{concurrency} workers took {total_time:.2f} seconds to complete {len(results)} requests"
-    )
+    return total_time
+    # print("---")
+    # print(
+    #     f"{concurrency} workers took {total_time:.2f} seconds to complete {len(results)} requests"
+    # )
 
 
 def assault(url, requests, concurrency):
     """ Entrypoint to making requests """
     results = []
-    asyncio.run(
+    total_time = asyncio.run(
         distribute_work(url, requests, concurrency, results)
     )  # distribute_work doesn't return result, but returns a coroutine, that asyncio.run() will cause to execute
-    print(results)
+    return (total_time, results)
+    # print(results)
